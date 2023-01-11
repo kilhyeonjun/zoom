ee94d87f2c2424ed26d1f40a825a2b83f114ef01
kilhyeonjun <kboxstar@gmail.com>
2023-01-11 17:31:33 +0900

    add nickname

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
@@ -18,14 +20,21 @@ function onSocketClose() {
 }
 
 const sockets = [];
+
 wss.on('connection', (socket) => {
   sockets.push(socket);
+  socket['nickname'] = 'Anon';
   console.log('Connected to Browser ✅');
   socket.on('close', onSocketClose);
-  socket.on('message', (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message));
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
   });
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
