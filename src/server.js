0a0ecf6ee759161960dd37fe02555e092bc73cd7
kilhyeonjun <kboxstar@gmail.com>
2023-01-12 17:43:05 +0900

    add show camera & mic

--- src/server.js
+++ src/server.js
@@ -17,24 +17,5 @@ wsServer.on('connection', (socket) => {
   console.log(socket);
 });
 
-/*
-const wss = new WebSocket.Server({server});
-const sockets = [];
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anon';
-  console.log('Connected to Browser ✅');
-  socket.on('close', onSocketClose);
-  socket.on('message', (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case 'new_message':
-        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-      case 'nickname':
-        socket['nickname'] = message.payload;
-    }
-  });
-});*/
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
