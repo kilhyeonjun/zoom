ef57d60c775efd3963c783f378925119ddb46e63
kilhyeonjun <kboxstar@gmail.com>
2023-01-12 11:40:58 +0900

    add join Room

--- src/server.js
+++ src/server.js
@@ -14,10 +14,24 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-  console.log(socket);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on('enter_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit('welcome');
+  });
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) => socket.to(room).emit('bye'));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', msg);
+    done();
+  });
 });
 
-/*
+/* 
 const wss = new WebSocket.Server({server});
 const sockets = [];
 wss.on('connection', (socket) => {
